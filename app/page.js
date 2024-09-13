b79dbc78ed54df3c63e327e107cea357c6b5db9c
MyNamSam <144481308+MyNamSam@users.noreply.github.com>
2024-09-12 23:22:31 -0600

    Week-2 Assignment
    Added pages, student-info.
    Edited app\page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
+import Page from "./week-2/page";
 
 
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2/">Week 2</Link>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return (
+      <div>
+        <h2>Samarpan Magar</h2>
+        <Link href="https://github.com/MyNamSam/cprg306-assignments">My GitHub Repository</Link>
+      </div>
+    );
+  }
