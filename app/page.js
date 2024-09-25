ba00efe7faad1a4e7a70c5aa490c72e2a944eb61
MyNamSam <144481308+MyNamSam@users.noreply.github.com>
2024-09-25 13:29:52 -0600

    Week 3 Assignment
    Created week-3 folder w/ item, item-list, and page files

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Page from "./week-2/page";
 
 
 
@@ -7,7 +6,14 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="./week-2/">Week 2</Link>
+      <ul>
+        <li>
+          <Link href="./week-2/">Week 2</Link>
+        </li>
+        <li>
+          <Link href="./week-3/">Week 3</Link>
+        </li>
+      </ul>
     </main>
   );
 }
---
+++ app/week-3/item.js
@@ -0,0 +1,13 @@
+
+
+
+export default function Item({name, quantity, category}) {
+    
+  return (
+    <div class="p-2 m-4 bg-pink-200 border-4 border-green-900 max-w-60">
+      <h2 class="font-bold">{name}</h2>
+      <p>Quantity: {quantity}</p>
+      <p>Category: {category}</p>
+    </div>
+  );
+}
